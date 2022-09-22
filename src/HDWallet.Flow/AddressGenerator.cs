c7419fb3c8f8176d11c1e76f149455d31744a84d
Emre Pehlivan <emre.pehlivan@btctrader.com>
2022-09-22 14:08:12 +0300

    fix test and add comment

--- src/HDWallet.Flow/AddressGenerator.cs
+++ src/HDWallet.Flow/AddressGenerator.cs
@@ -6,6 +6,11 @@ namespace HDWallet.Flow
 {
     public class AddressGenerator : IAddressGenerator
     {
+        /// <summary>
+        /// Exceptionally, this method returns public key for FLOW instead of address. Address should be generated on chain with this public key.
+        /// </summary>
+        /// <param name="pubKeyBytes"></param>
+        /// <returns></returns>
         public string GenerateAddress(byte[] pubKeyBytes)
         {
             var pubKey = new PubKey(pubKeyBytes);
