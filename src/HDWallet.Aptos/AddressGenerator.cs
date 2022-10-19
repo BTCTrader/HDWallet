11da62f86fbf608c3bddfdd3ece9b4080764f5ed
Onur Uyanik <onuyanik@protonmail.com>
2022-10-19 20:41:50 +0300

    Added hexadecimal prefix "0x" for address

--- src/HDWallet.Aptos/AddressGenerator.cs
+++ src/HDWallet.Aptos/AddressGenerator.cs
@@ -14,14 +14,14 @@ namespace HDWallet.Aptos
             }
 
             byte[] zeroAppendedPubKeyBytes = new byte[33];
-            System.Buffer.BlockCopy(pubKeyBytes, 0, zeroAppendedPubKeyBytes, 0, 32);
+            Buffer.BlockCopy(pubKeyBytes, 0, zeroAppendedPubKeyBytes, 0, 32);
             zeroAppendedPubKeyBytes[32] = 0x00;
 
             IHash hashFnAptos = SharpHash.Base.HashFactory.Crypto.CreateSHA3_256();
             byte[] sha3256 = hashFnAptos.ComputeBytes(zeroAppendedPubKeyBytes).GetBytes();
-            string computedSha3256Bytes = sha3256.ToHexString();
+            string address = sha3256.ToHexString();
 
-            return computedSha3256Bytes;
+            return $"0x{address}";
         }
     }
 }
--- test/HDWallet.Aptos.Tests/GenerateAptosWallet.cs
+++ test/HDWallet.Aptos.Tests/GenerateAptosWallet.cs
@@ -25,15 +25,15 @@ namespace HDWallet.Aptos.Tests
             TestContext.Out.WriteLine("PublicKey: {0}", wallet.PublicKeyBytes.ToHexString());
             TestContext.Out.WriteLine("PrivateKey: {0}", wallet.PrivateKeyBytes.ToHexString());
 
-            Assert.AreEqual(expected: "2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", wallet.Address);
+            Assert.AreEqual(expected: "0x2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", wallet.Address);
             Assert.AreEqual(expected: "be8b1f4e71c8886b63c8e1fa63a7d57170dfdcf6cf7a9922b98fbb217791636b", wallet.PublicKeyBytes.ToHexString());
             Assert.AreEqual(expected: "dea10d9e27387f66e3d271bdb92fe861c20c9427e09e3692c2aae2f73480b7c2", wallet.PrivateKeyBytes.ToHexString());
             
             var helloBytes = System.Text.Encoding.ASCII.GetBytes("Hello Dear World!");
             var sig = wallet.Sign(helloBytes);
-            var sighex = sig.R.ToHexString() + sig.S.ToHexString();
-            TestContext.Out.WriteLine("Signature: {0}", sighex);
-            Assert.AreEqual(expected: "b55d4fb7c0c3437ea43c9ad5ceb9644be2dda7fd16c21c359dabfcfaeaf9c93a0ceaf99e8741200e8e1bf0080f838c6b55ecb0d3dae0aadafb6cf85e2d841608", sighex);
+            var sigHex = sig.R.ToHexString() + sig.S.ToHexString();
+            TestContext.Out.WriteLine("Signature: {0}", sigHex);
+            Assert.AreEqual(expected: "b55d4fb7c0c3437ea43c9ad5ceb9644be2dda7fd16c21c359dabfcfaeaf9c93a0ceaf99e8741200e8e1bf0080f838c6b55ecb0d3dae0aadafb6cf85e2d841608", sigHex);
         }
 
         [Test]
@@ -46,7 +46,7 @@ namespace HDWallet.Aptos.Tests
             Assert.AreEqual("xprv9s21ZrQH143K4E3XC8LB5AZe82VydyfN779qMSV8syL5tEaPgCT3PBu98Fs6sqhYZMpCUf8WZ5Zpt647X9PLoEvFYeFzd43zxE5EoTPgjQJ", account0.GetWif());
 
             var wallet = account0.GetExternalWallet(1);
-            Assert.AreEqual(expected: "2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", wallet.Address);
+            Assert.AreEqual(expected: "0x2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", wallet.Address);
 
             string xprv = "xprv9s21ZrQH143K4E3XC8LB5AZe82VydyfN779qMSV8syL5tEaPgCT3PBu98Fs6sqhYZMpCUf8WZ5Zpt647X9PLoEvFYeFzd43zxE5EoTPgjQJ";
             ExtKey masterKey = ExtKey.CreateFromWif(xprv);
@@ -54,11 +54,11 @@ namespace HDWallet.Aptos.Tests
             var actualWallet = account0FromXprv.GetExternalWallet(1);
 
             Assert.AreEqual(wallet.PublicKeyBytes, actualWallet.PublicKeyBytes);
-            Assert.AreEqual(expected: "2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", actualWallet.Address);
+            Assert.AreEqual(expected: "0x2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", actualWallet.Address);
 
             account0FromXprv = new Account<AptosWallet>(xprv);
             actualWallet = account0FromXprv.GetExternalWallet(1);
-            Assert.AreEqual(expected: "2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", actualWallet.Address);
+            Assert.AreEqual(expected: "0x2a692bd8275bf63683b54a69a103ba07086bfc69ad82486ed432704bde363942", actualWallet.Address);
         }
     }
 }
